3f229459774335f80ac4b54d4e577f97c3b15fab
Puripan22 <poohvs555@gmail.com>
2023-10-11 22:07:00 +0700

    Test

--- client/safePassage/src/App.jsx
+++ client/safePassage/src/App.jsx
@@ -1,36 +1,31 @@
 import { useState } from 'react'
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
+    
     <div>
-      <div className="status">
-        <div className = "Profile">
+      <div className='status'>
+        <div className = 'Profile'>
           <img src="" alt="" />
+          <p className='welcome'>Welcome <p className = 'user'> user </p> <p className = 'address'> kb 23/256 </p> </p>
         </div>
-        <p>Welcome</p>
-      <div className = "username">
-        <p className = "user"> </p>
-        <p className = "address"> </p>
       </div>
-
+      
+      <div className='select'>
+        <h1>
+        <button className = 'visitor' hrel = "#"> Visitor </button>
+        </h1>
+        <h2>
+        <button className = 'delivery' hrel = "#"> Delivery </button>
+        </h2>
+        <h3>
+        <button className = 'houseowner' hrel = "#"> House Owner </button >
+        </h3>
       </div>
-      <h1>
-        <button className = "visitor" hrel = "#"> Visitor </button>
-      </h1>
-      <h2>
-        <button className = "delivery" hrel = "#"> Delivery </button>
-      </h2>
-      <h3>
-        <button className = "houseowner" hrel = "#"> House Owner </button >
-      </h3>
-=======
-    <div className=''>
-
->>>>>>> e896eb76e1b058aa11506e264fe5330e1b848e03
     </div>
   )
 }

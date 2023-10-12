796f7c42c01b29608e2e51930f5123ec84717e20
KITTICHAI <junoir151040@gmail.com>
2023-10-12 14:08:57 +0700

    เพิ่ม libary และเชื่อมต่อ DB

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require ('cors');
+const mysql = require('mysql2/promise');
+const dbHost = process.env.HOST;
+const dbPassword = process.env.PASSWORD;
+
+const app = express();
+
+app.use((req,res,next)=>{
+    next()
+});
+app.use(express.json());
+app.use(cors({}));
+
+const connection = async ()=>{
+    const db = await mysql.createConnection({
+        user:"admin",
+        host:dbHost,
+        password:dbPassword,
+        database:"Villa"
+    })
+    return db;
+};
+
+app.get('/getData',async (req, res)=>{
+    
+    const db = await connection();
+    const result = await db.query('select * from houseOwner')
+    const data = result[0] 
+    res.json(data)
+});
+
+
+app.listen(4444,()=>{
+    console.log("server is running")
+});
+

5d26e50b2b8e87f4a3fdb9417a27eaff427bb682
jeagerism <oo4dxuser@gmail.com>
2023-10-14 16:57:13 +0700

    home page

--- client/safePassage/src/Home.jsx
+++ client/safePassage/src/Home.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import Model from './assets/FreeVector-Modern-House.ai.png'
 
 function Home() {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
     const loginBtn = (e) => {
         e.preventDefault();
         navigate('/security/login');
     };
 
     return (
-        <div>
-            <button onClick={loginBtn}>Login</button>
+        <div className='px-24'>
+            <Navbar/>
+            <div className=' flex mt-32 justify-between'>
+            <div className=''>
+                <p className=' text-7xl text-slate-800'>A <span className=' font-bold'>super solution</span> <br />for your <span className=' font-bold'>business.</span> </p>
+
+                <div className=' my-10'>
+                    <p className=' text-xl text-slate-600'>
+                        Our maketing and sales automations <br />
+                        help you scale your outreach to get <br />
+                        more leads for your company.
+                    </p>
+                </div>
+                <button onClick={loginBtn} className=' bg-slate-800 text-white px-8 py-4 rounded-md'>Request Beta Access</button>
+            </div>
+            
+            <div className="w-1/2">
+                <img src={Model} alt="" className='' />
+            </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
